6ed55778be0ecd22ab23f7f60b171cade4cebf71
vikas saparam <vsaparam@ford.com>
2025-07-15 16:47:07 +0000

    add task/delete task functionality

--- webapp/controller/newTask.controller.js
+++ webapp/controller/newTask.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
+    "sap/m/MessageBox",
     "sap/m/MessageToast"
-], (Controller, JSONModel, MessageToast) => {
+], (Controller, JSONModel, MessageBox, MessageToast) => {
     "use strict";
 
     return Controller.extend("com.practise.todolist.todolist.controller.newTask", {
@@ -15,9 +16,47 @@ sap.ui.define([
         },
 
         onAddTask(oEvent){
+            let message = "";
             const sTaskTitle = this.getView().byId("newTaskTitle").getValue();
             if(sTaskTitle ===null | sTaskTitle.length == 0)
-                MessageToast.show("Task title cannot be empty");
+                message = message + "Task title cannot be empty!";
+            
+            const dDueDate = this.getView().byId("newTaskDueDate").getDateValue();
+            if (!dDueDate) {
+                message += " Due date cannot be empty!";
+            }else{
+                const currentDate = new Date();
+                currentDate.setHours(0,0,0,0);
+                if(currentDate > dDueDate){
+                    message += " Due date cannot be in the past!"
+                }
+            }
+
+            if (message) {
+                MessageBox.error(message);
+            }else{
+                // validations passed
+                const oView = this.getView();
+                const oDataModel = oView.getModel("toDoItems");
+                const aTasks = oDataModel.getData();
+                const sTaskDescription = oView.byId("newTaskDescription").getValue();
+                const sTaskPriority = oView.byId("newTaskPriority").getProperty("value");
+                const sTaskCategory = oView.byId("newTaskCategory").getProperty("value");
+                const dDueDate = oView.byId("newTaskDueDate").getValue();
+                aTasks.push({ title : sTaskTitle,
+                              description : sTaskDescription,
+                              priority : sTaskPriority,
+                              category : sTaskCategory,
+                              dueDate : dDueDate });
+                oDataModel.setData(aTasks);
+                MessageToast.show("Task has been added!");
+                oView.byId("newTaskTitle").setValue("");
+                oView.byId("newTaskDescription").setValue("");
+                oView.byId("newTaskPriority").setSelectedItem(null);
+                oView.byId("newTaskCategory").setSelectedItem(null);
+                oView.byId("newTaskDueDate").setDateValue(null)
+                
+            }
         }
     });
 });
--- webapp/controller/toDoList.controller.js
+++ webapp/controller/toDoList.controller.js
@@ -7,9 +7,21 @@ sap.ui.define([
 
     return Controller.extend("com.practise.todolist.todolist.controller.toDoList", {
         formatter : formatter,
-        onInit() {
-            const oModel = new JSONModel("../toDo.json", true);
-            this.getView().setModel(oModel,"toDoItems");
+        // onInit() {
+        //     const oModel = new JSONModel("../toDo.json", true);
+        //     this.getView().setModel(oModel,"toDoItems");
+        // }
+
+        onDeleteTask(oEvent){
+            const index = oEvent.getSource().oPropagatedProperties.oBindingContexts.toDoItems.getPath().split("/").pop();
+            const oModel = this.getView().getModel("toDoItems");
+            const aTasks = oModel.getData();
+            aTasks.splice(index, 1);
+            oModel.setData(aTasks);
+        },
+
+        onItemPress(){
+            
         }
     });
 });
